58e99bf6ef08397627704fb731d1acb44bc62cd8
sookyung.kim <sookyung.k.kim@gmail.com>
2022-11-23 16:32:20 +0900

    roadmap, lecture common source create

---
+++ src/main/java/com/spring/learn/roadmap/RoadmapService.java
@@ -0,0 +1,5 @@
+package com.spring.learn.roadmap;
+
+public interface RoadmapService {
+
+}
---
+++ src/main/java/com/spring/learn/roadmap/impl/RoadmapDAO.java
@@ -0,0 +1,10 @@
+package com.spring.learn.roadmap.impl;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoadmapDAO {
+
+}
